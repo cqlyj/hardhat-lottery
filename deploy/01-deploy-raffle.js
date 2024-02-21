4150f8a0335a27c6b205e81fe0ce94762fb44848
cqlyj <luoyingjie0721@gmail.com>
2024-02-21 15:53:50 +0800

    Add deploy scripts of Raffle and Mock

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,60 @@
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify.js");
+
+const VRF_SUB_FUND_AMOUNT = ethers.parseUnits("1", 18);
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.target;
+
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1);
+    subscriptionId = transactionReceipt.logs[0].args[0];
+
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+    subscriptionId = networkConfig[chainId]["subscriptionId"];
+  }
+
+  const args = [
+    vrfCoordinatorV2Address,
+    networkConfig[chainId]["entranceFee"],
+    networkConfig[chainId]["gasLane"],
+    subscriptionId,
+    networkConfig[chainId]["callbackGasLimit"],
+    networkConfig[chainId]["keepersUpdateInterval"],
+  ];
+
+  const raffle = await deploy("Raffle", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfrimations: network.config.blockConfirmations || 1,
+  });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log("Verifying...");
+    await verify(raffle.address, arguments);
+  }
+
+  log("_________________________________");
+};
+
+module.exports.tags = ["all", "raffle"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,34 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+    player: {
+      default: 1,
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 11155111,
+    },
+  },
 };
